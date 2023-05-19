7265cd29a4e21f376964a76c1bd1ea7da1740bf4
c22mohel <c22mohel@student.his.se>
2023-05-19 13:51:49 +0200

    Har lagt till en about sida samt en knapp som går till den

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Definerar knappen about
+    Button about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        //länkar about button med ID
+        about = findViewById(R.id.AboutButton);
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchAbout();
+            }
+        });
+    }
+
+    private void switchAbout(){
+        Intent AboutIntent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(AboutIntent);
     }
 
 }
